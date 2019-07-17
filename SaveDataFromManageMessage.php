9facde954bee1f51ace05ca8795c0813429dcead
Brehboyy <ousmane16diarra@gmail.com>
2019-07-17 09:45:56 +0200

    Version de qualité

--- SaveDataFromManageMessage.php
+++
@@ -1,8 +0,0 @@
-
-<?php ?>
-<script type="text/javascript">
-$("#selectsearch").change(function() {
-  var action = $(this).val() == "people" ? "user" : "content";
-  $("#search-form").attr("action", "/search/" + action);
-});
-</script>
