3654a81e78e562563c3184df19537514a626e67f
Brehboyy <ousmane16diarra@gmail.com>
2019-07-18 15:41:11 +0200

    Ajout des tag dans edit

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
-let baseUrl = 'http://localhost:80/apifidelia/tests/public/';
+let baseUrl = 'http://localhost:8888/apifidelia/tests/public/';
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+        }
+    }
+};
 (function ($) {
     "use strict";
-
-    
     /*==================================================================
     [ Validate ]*/
     var input = $('.validate-input .input100');
