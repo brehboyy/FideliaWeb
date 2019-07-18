eef5a3115438dedc4da34a119044047bacbb1b25
Samson <samsonrobin30@gmail.com>
2019-07-18 20:46:39 +0200

    Verify filled value

--- js/accueil.js
+++ js/accueil.js
@@ -18,9 +18,9 @@
                 output += '<td class="pt-3-half" contenteditable="false">' + val.Date_Modele_Message + '</td>';
                 output += '<td class="pt-3-half" contenteditable="false" width="150px">'                        
                 output += '<select class="browser-default custom-select test" id="statut">'
-                output += '<option name="teste" value="EN COURS"  '+( val.Statut_Message == "EN COURS" ? 'selected' : '')+'>EN COURS</option>';
-                output += '<option value="ARCHIVE" '+( val.Statut_Message == "ARCHIVE" ? 'selected' : '')+'>ARCHIVE</option>';
-                output += '<option value="EN ATTENTE" '+( val.Statut_Message == "EN ATTENTE" ? 'selected' : '')+'>EN ATTENTE</option>';
+                output += '<option value="EN COURS,'+val.ID_Modele_Message+'"  '+( val.Statut_Message == "EN COURS" ? 'selected' : '')+'>EN COURS</option>';
+                output += '<option value="ARCHIVE,'+val.ID_Modele_Message+'" '+( val.Statut_Message == "ARCHIVE" ? 'selected' : '')+'>ARCHIVE</option>';
+                output += '<option value="EN ATTENTE,'+val.ID_Modele_Message+'" '+( val.Statut_Message == "EN ATTENTE" ? 'selected' : '')+'>EN ATTENTE</option>';
                 output += '</select></td>';
                 output += '<td> <span class="table-remove"><button onclick="deleteRow('+ val.ID_Modele_Message+ ')" type="button" class="btn btn-danger btn-rounded btn-sm my-0">Supprimer</button></span></td>';
                 output += '<td> <span class="table-remove"><a href="CreationMailPage.html?ID_Modele_Message='+val.ID_Modele_Message+'" type="button" class="indigo-text"><i class="fas fa-edit" aria-hidden="true"></i></a></span></td>';
@@ -43,12 +43,39 @@
     //=========================END Dropdown filter table ==========================
 
 })(jQuery);
+    function showValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).addClass('alert-validate');
+    }
+    function validate (input) {
+        if(input.localName == 'input'){
+            if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+                if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+                    return false;
+                }
+            }else if ($(input).attr('name') == 'login' || ($(input).attr('name') == 'pass')){
+                if($(input).val().trim().length < 4)
+                return false;
+            }
+            else {
+                if($(input).val().trim() == ''){
+                    return false;
+                }
+            }
+        }
+    }
 
       $(document).on('change', '#statut', function() {  
-           Statut_Message =$(this).val();  
-           console.log(Statut_Message);
-                           $.ajax({
-                    url: baseUrl + 'message.php/updateMessage',
+           Message =$(this).val();  
+           console.log(Message);
+           var res = Message.split(",");
+           let messageObject = {
+                'Id': res[1],
+                'Status': res[0]
+            };
+                $.ajax({
+                    url: baseUrl + 'message.php/updateStatusMessage',
                     type: 'POST',
                     dataType: 'json',
                     data: {
@@ -56,7 +83,6 @@
                     },
                     cache: false
                 }).done(function(response) {
-
                     console.log(response);
                        let resultat;
                         try {
@@ -110,7 +136,15 @@
       });
       
       $('#sauvegarder').submit(function(){
-           console.log(result);
+        let inputIns = $('#sauvegarder :input');
+          alert(inputIns);
+        var check = true;
+        for(var i=0; i<inputIns.length; i++) {
+            if(validate(inputIns[i]) == false){
+                showValidate(inputIns[i]);
+                check=false;
+            }
+        }
           var selectCondition = $( "#selectCondition option:selected" ).text();
           if (selectCondition = 'Condition') {selectCondition = 'Aucune condition'}
           var numberFrequence = $('#numberFrequence').val();
